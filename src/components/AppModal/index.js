b0635fcef6a38667179083dddea5db5ff8602559
Michael Pfohl <rmpfohl@gmail.com>
2020-12-05 11:23:36 -0600

    create outing complete

---
+++ src/components/AppModal/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import {
+  Button, Modal, ModalHeader, ModalBody, ModalFooter,
+} from 'reactstrap';
+
+const AppModal = (props) => {
+  const { buttonLabel, className, title } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  return (
+    <div>
+      <Button color="danger" onClick={toggle} className="mt-4">
+        {buttonLabel}
+      </Button>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>{title}</ModalHeader>
+        <ModalBody>
+          {props.children}
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default AppModal;
--- src/views/Outings.js
+++ src/views/Outings.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
+import AppModal from '../components/AppModal';
+import OutingForm from '../components/Forms/OutingForm';
+
 class Outings extends Component {
-  state = {};
+  state = {
+    outings: [],
+  };
 
   render() {
     return (
       <div className="outings--container">
         <h1 className="outings--header">Outings</h1>
+        <AppModal title={'Create Outing'} buttonLabel={'Create Outing'}>
+          <OutingForm />
+        </AppModal>
       </div>
     );
   }
