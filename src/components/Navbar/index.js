553d281cc63ddd240bfc3b427b94f3be0c3d35ee
Michael Pfohl <rmpfohl@gmail.com>
2020-12-22 19:02:58 -0500

    make navbar collapse at 992px

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -30,7 +30,7 @@ const MainNavbar = (props) => {
   const { user, experience } = props;
   return (
     <div>
-      <Navbar className="main--navbar" expand="md">
+      <Navbar className="main--navbar" expand="lg">
       <NavbarBrand href="/" className="d-flex">
         <img className="main-logo" src={logo} alt="nature by Creative Mania from the Noun Project"/>
         <div className="nav--header">Gnophy</div>
